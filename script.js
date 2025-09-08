949a01d7e3a2ed9a5ef1d299af6ba23a44b3f654
Abinan Jeyachandran <abinan.jeyachandran@gmail.com>
2025-09-08 17:06:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-// Theme toggle
+// Dark/Light theme toggle
 const toggleBtn = document.getElementById("theme-toggle");
 toggleBtn.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 
-  // Change button icon
-  if (document.body.classList.contains("dark")) {
-    toggleBtn.textContent = "☀️";
-  } else {
-    toggleBtn.textContent = "🌙";
-  }
+  // Switch icon
+  toggleBtn.textContent = document.body.classList.contains("dark") ? "☀️" : "🌙";
 });
